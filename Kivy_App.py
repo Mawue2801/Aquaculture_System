5f42cd1ba0644b3dfd3e69bf0307c3aab3ac2381
unknown <mawue2801@gmail.com>
2021-10-01 11:02:24 +0000

    Added System Settings Screen

--- Kivy_App.py
+++ Kivy_App.py
@@ -24,32 +24,36 @@ Window.size = (800,480)
 class ListItemWithCheckbox(OneLineAvatarIconListItem):
     icon = StringProperty("delete")
 
+class ListItemWithCheckbox2(OneLineAvatarIconListItem):
+    icon = StringProperty("delete")
+
 class CredentialsContent(BoxLayout):
     pass
 
 
 class Example(MDApp):
     dialog1 = None
+    dialog2 = None
+    dialog3 = None
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         global screen_manager
         screen_manager = ScreenManager()
-        screen_manager.add_widget(Builder.load_file("MainScreen.kv"))
         screen_manager.add_widget(Builder.load_file("LoginScreen.kv"))
-        self.selected_pond = "1"
+        screen_manager.add_widget(Builder.load_file("SystemSettingsScreen.kv"))
+        screen_manager.add_widget(Builder.load_file("MainScreen.kv"))
+        self.selected_pond = "Pond 1"
         self.securityQuestion = ""
         self.securityAnswer = ""
+        self.selected_species_2 = "Nile Tilapia"
         self.selected_species = "Nile Tilapia"
         self.username = "smart aquapak"
         self.password = "fish1234"
-        # self.minDissolvedOxygen = 4
-        # self.maxDissolvedOxygen = 8
-        # self.minpH = 7.5
-        # self.maxpH = 8.5
-        # self.salinity = 0.5
-        # self.minConductivity = 30
-        # self.maxConductivity = 5000
+        self.setSystem = False
+        self.approveSpeciesChange = False
+        self.num_of_ponds = 0
         self.RedStatus = 0
+        self.pondDict = {}
         self.feedingList = []
 
         self.TilapiaFeeding = {
@@ -122,21 +126,6 @@ class Example(MDApp):
                             1037:[10.4,32,5,1],
                             1073:[10.7,32,5,1],
         }
-                       
-        pond_menu_items = [
-            {
-                "viewclass": "OneLineListItem",
-                "text": f"Pond {i}",
-                "height": dp(56),
-                "on_release": lambda x=f"Pond {i}": self.set_pond_item(x),
-            } for i in range(1,3)
-        ]
-        self.pond_menu = MDDropdownMenu(
-            caller=screen_manager.get_screen("MainScreen").ids.pond_selection,
-            items=pond_menu_items,
-            width_mult=4,
-        )
-        self.pond_menu.bind()
         
         species = ["Nile Tilapia", "Catfish", "Shrimp", "Prawn"]
         species_menu_items = [
@@ -154,13 +143,34 @@ class Example(MDApp):
         )
         self.species_menu.bind()
 
+        # Species Menu for System Settings Page
+        species_menu_items_2 = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": species[i],
+                "height": dp(56),
+                "on_release": lambda x=species[i]: self.set_species_item_2(x),
+            } for i in range(4)
+        ]
+        self.species_menu_2 = MDDropdownMenu(
+            caller=screen_manager.get_screen("SystemSettingsScreen").ids.species_selection,
+            items=species_menu_items_2,
+            width_mult=4,
+        )
+        self.species_menu_2.bind()
+
     def set_pond_item(self, text_item):
         screen_manager.get_screen("MainScreen").ids.pond_selection.set_item(text_item)
-        self.selected_pond = text_item.split(" ")[1]
+        #self.selected_pond = text_item.split(" ")[1]
+        self.selected_pond = text_item
         self.pond_menu.dismiss()
+        self.selected_species = self.pondDict[text_item][0]
+        self.root.get_screen("MainScreen").ids.fish_count.text = self.pondDict[text_item][1]
+        screen_manager.get_screen("MainScreen").ids.species_selection.set_item(self.pondDict[text_item][0])
 
     def set_species_item(self, text_item):
         screen_manager.get_screen("MainScreen").ids.species_selection.set_item(text_item)
+        self.pondDict[self.selected_pond][0] = text_item
         self.selected_species = text_item
         if self.selected_species == "Nile Tilapia":
             self.root.get_screen("MainScreen").ids.minTemp.text = "12"
@@ -175,43 +185,116 @@ class Example(MDApp):
             self.root.get_screen("MainScreen").ids.minTemp.text = "14"
             self.root.get_screen("MainScreen").ids.maxTemp.text = "36"
         self.species_menu.dismiss()
+        self.species_change_alert()
 
+    def set_species_item_2(self, text_item):
+        screen_manager.get_screen("SystemSettingsScreen").ids.species_selection.set_item(text_item)
+        self.selected_species_2 = text_item
+        self.species_menu_2.dismiss()
+    
+    def species_change_alert(self):
+        if not self.dialog3:
+            self.dialog3 = MDDialog(
+                text="Species for Selected Pond Changed",
+                buttons=[
+                    MDFlatButton(
+                        text="OK", text_color=self.theme_cls.primary_color,on_release=self.close_dialog3
+                    ),
+                ],
+            )
+        self.dialog3.open()
+
+    def close_dialog3(self,obj):
+        self.dialog3.dismiss()
+    
     def build(self):
         self.theme_cls.primary_palette = "Blue"
-        try:
-            ports = serial.tools.list_ports.comports()
-            for i in ports:
-                i = str(i)
-                if 'Arduino' in i:
-                    commPort = i.split(' ')[0]
-                if commPort != 'None':
-                    self.arduino = serial.Serial(commPort,baudrate = 9600, timeout=1)
-                    print('Connected to ' + commPort)
-        except:
-            print('Connection Issue!')
+        if self.setSystem == True:
+            try:
+                ports = serial.tools.list_ports.comports()
+                for i in ports:
+                    i = str(i)
+                    if 'Arduino' in i:
+                        commPort = i.split(' ')[0]
+                    if commPort != 'None':
+                        self.arduino = serial.Serial(commPort,baudrate = 9600, timeout=1)
+                        print('Connected to ' + commPort)
+            except:
+                print('Connection Issue!')
         # Clock.schedule_interval(self.update, 1)
         Clock.schedule_interval(self.update_time, 1)
-        
         return screen_manager
+
+    def set_system(self):
+        self.num_of_ponds = len(self.pondDict)
+        pond_menu_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": f"Pond {i}",
+                "height": dp(56),
+                "on_release": lambda x=f"Pond {i}": self.set_pond_item(x),
+            } for i in range(1,self.num_of_ponds+1)
+        ]
+        self.pond_menu = MDDropdownMenu(
+            caller=screen_manager.get_screen("MainScreen").ids.pond_selection,
+            items=pond_menu_items,
+            width_mult=4,
+        )
+        self.pond_menu.bind()
+
+        screen_manager.current = "MainScreen"
+        name = self.username 
+        toast(f"Welcome {name}")
+        self.selected_species = self.pondDict["Pond 1"][0]
+        self.root.get_screen("MainScreen").ids.fish_weight.text = self.pondDict["Pond 1"][1]
+        screen_manager.get_screen("MainScreen").ids.species_selection.set_item(self.pondDict["Pond 1"][0])
 
     def log_in(self):
         if self.root.get_screen("LoginScreen").ids.user.text == self.username and self.root.get_screen("LoginScreen").ids.password.text == self.password:
-           name = self.root.get_screen("LoginScreen").ids.user.text
-           toast(f"Welcome {name}")
-           screen_manager.current = "MainScreen" 
-           self.root.get_screen("LoginScreen").ids.user.text = ""
-           self.root.get_screen("LoginScreen").ids.password.text = ""
-
-        else:
+           if self.setSystem == False:
+               screen_manager.current = "SystemSettingsScreen" 
+               self.root.get_screen("LoginScreen").ids.user.text = ""
+               self.root.get_screen("LoginScreen").ids.password.text = ""
+               self.setSystem = True
+           else:
+                name = self.username
+                toast(f"Welcome {name}")
+                screen_manager.current = "MainScreen" 
+                self.root.get_screen("LoginScreen").ids.user.text = ""
+                self.root.get_screen("LoginScreen").ids.password.text = ""
+        else:                           
             toast("Invalid Username or Password")
-
+            
     def log_out(self):
         screen_manager.current = "LoginScreen"
     
     def get_time(self,instance,time):
         self.root.get_screen("MainScreen").ids.feeding_time_label.text = str(time)
     
-    def add_to_list(self):
+    def add_to_ponds_dict(self):
+        self.num_of_ponds += 1
+        name = "Pond " + str(self.num_of_ponds)
+        species = self.selected_species_2
+        count = self.root.get_screen("SystemSettingsScreen").ids.fish_count.text
+
+        if species == "Catfish":
+            species += "       "
+        elif species == "Shrimp":
+            species += "       "
+        elif species == "Prawn":
+            species += "         "
+
+        if len(count) == 1:
+            self.root.get_screen("SystemSettingsScreen").ids.pond_identities_list.add_widget(ListItemWithCheckbox2(text=f"{name}                    {species}                   {count}"))
+        elif len(count) == 2:
+            self.root.get_screen("SystemSettingsScreen").ids.pond_identities_list.add_widget(ListItemWithCheckbox2(text=f"{name}                    {species}                   {count}"))
+        elif len(count) == 3:
+            self.root.get_screen("SystemSettingsScreen").ids.pond_identities_list.add_widget(ListItemWithCheckbox2(text=f"{name}                    {species}                   {count}"))
+        elif len(count) == 4:
+            self.root.get_screen("SystemSettingsScreen").ids.pond_identities_list.add_widget(ListItemWithCheckbox2(text=f"{name}                    {species}                   {count}"))
+        self.pondDict[name] = [self.selected_species_2,count]
+
+    def add_to_feeding_list(self):
         time_dict = {"00":"twelve","01":"one","02":"two","03":"three","04":"four","05":"five","06":"six",
                     "07":"seven","08":"eight","09":"nine","10":"ten","11":"eleven","12":"twelve","13":"one",
                     "14":"two","15":"three","16":"four","17":"five","18":"six","19":"seven","20":"eight",
@@ -260,12 +343,18 @@ class Example(MDApp):
         self.feedingList.remove(widget.text)
         print(self.feedingList)
         
+    def remove_widget_2(self,widget):
+        name = widget.text.split(" ")[0]
+        name = name + " " + widget.text.split(" ")[1]
+        self.root.get_screen("SystemSettingsScreen").ids.pond_identities_list.remove_widget(widget)
+        del self.pondDict[name]
+        print(self.pondDict)
     
     # def update(self, *args):
     #     arduino = self.arduino
     #     data = str(arduino.readline(arduino.inWaiting()).decode()).split(";")
     #     try:
-    #         data = data[int(self.selected_pond)-1].split(",")
+    #         data = data[int(self.selected_pond.split(" ")[1])-1].split(",")
     #         self.root.get_screen("MainScreen").ids.do_label.text = data[0]
     #         self.root.get_screen("MainScreen").ids.temp_label.text = data[1]
     #         self.root.get_screen("MainScreen").ids.pH_label.text = data[2]
@@ -290,42 +379,42 @@ class Example(MDApp):
     #         pass
 
     def change_login_details_dialog(self):
-        if not self.dialog1:
-            self.dialog1 = MDDialog(
+        if not self.dialog2:
+            self.dialog2 = MDDialog(
                 type="custom",
                 content_cls=CredentialsContent(),
                 buttons=[
                     MDFlatButton(
-                        text="CANCEL", text_color=self.theme_cls.primary_color,on_release=self.close_dialog1
+                        text="CANCEL", text_color=self.theme_cls.primary_color,on_release=self.close_dialog2
                     ),
                     MDFlatButton(
                         text="OK", text_color=self.theme_cls.primary_color,on_release=self.change_login_details
                     ),
                 ],
             )
-        self.dialog1.open()
+        self.dialog2.open()
    
     def change_login_details(self,obj):
-        if self.dialog1.content_cls.ids.old_password.text == self.password:
-            if self.dialog1.content_cls.ids.new_password.text == self.dialog1.content_cls.ids.confirm_password.text:
-                self.username = self.dialog1.content_cls.ids.new_username.text
-                self.password = self.dialog1.content_cls.ids.new_password.text
+        if self.dialog2.content_cls.ids.old_password.text == self.password:
+            if self.dialog2.content_cls.ids.new_password.text == self.dialog2.content_cls.ids.confirm_password.text:
+                self.username = self.dialog2.content_cls.ids.new_username.text
+                self.password = self.dialog2.content_cls.ids.new_password.text
                 toast("Login Credentials Changed")
-                self.dialog1.dismiss()
-                self.dialog1.content_cls.ids.old_password.text = ""
-                self.dialog1.content_cls.ids.new_username.text = ""
-                self.dialog1.content_cls.ids.new_password.text = ""
-                self.dialog1.content_cls.ids.confirm_password.text = ""
+                self.dialog2.dismiss()
+                self.dialog2.content_cls.ids.old_password.text = ""
+                self.dialog2.content_cls.ids.new_username.text = ""
+                self.dialog2.content_cls.ids.new_password.text = ""
+                self.dialog2.content_cls.ids.confirm_password.text = ""
             else:
-                self.dialog1.content_cls.ids.confirm_password.text = ""
+                self.dialog2.content_cls.ids.confirm_password.text = ""
                 toast("Re-enter Confirmatory Password")
         else:
-            self.dialog1.content_cls.ids.old_password.text = ""
+            self.dialog2.content_cls.ids.old_password.text = ""
             toast("Invalid Old Password")
 
 
-    def close_dialog1(self,obj):
-        self.dialog1.dismiss()
+    def close_dialog2(self,obj):
+        self.dialog2.dismiss()
 
     def on_switch_active(self, instance, value):
         if value:
